22e1d1fe6508edfff455e7088a3e351a9a294aca
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-08 16:53:45 +0800

    feat: setup nextjs font optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Inter, Roboto_Mono } from "next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
+  display: "swap",
+  subsets: ["latin"],
+});
+
+const roboto_mono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  display: "swap",
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} ${roboto_mono.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
