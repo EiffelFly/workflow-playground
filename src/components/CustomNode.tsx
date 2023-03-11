f4dc0f4cff9f56a3002686a9aa9f50ccf9eb3263
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-11 17:09:04 +0800

    feat: update node style

--- src/components/CustomNode.tsx
+++ src/components/CustomNode.tsx
@@ -1,12 +1,27 @@
 import { NodeData } from "@/stores/workflow";
 import { memo } from "react";
 import { Handle, NodeProps, Position } from "reactflow";
+import { ResourceIcon } from "./ResourceIcon";
 
 export const CustomNode = memo(({ data }: NodeProps<NodeData>) => {
   return (
     <div className="px-4 py-2 rounded-[1px] bg-white border border-black">
-      <div className="flex font-sans text-[10px] font-medium text-black">
-        {data.type}
+      <div className="flex flex-col gap-y-1">
+        <div className="flex flex-row gap-x-2">
+          <ResourceIcon
+            type={data.type}
+            width="w-4"
+            height="h-4"
+            position="m-auto"
+            color="fill-black"
+          />
+          <p className="flex my-auto font-sans text-[10px] font-medium text-black">
+            {data.type}
+          </p>
+        </div>
+        <h3 className="text-black text-sm font-sans font-semibold">
+          {data.title}
+        </h3>
       </div>
       <Handle
         type="target"
